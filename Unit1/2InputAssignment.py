0be8243afc3cbff9a02ce15cf25cdc2422ef01f5
917Josh <dj917josh@gmail.com>
2024-03-29 12:48:10 -0500

    finished assignment 2

--- Unit1/2InputAssignment.py
+++ Unit1/2InputAssignment.py
@@ -1,15 +1,31 @@
 print("########## 1.2.1 ##########")
-
+name = input("Type your name: ")
+print (name)
+print (name)
 print("########## 1.2.2 ##########")
-
+name = input("Type your name: ")
+print ("What is your name?")
+print("!" + name + "!" + name + "!" )
 print("########## 1.2.3 ##########")
+firstname = input ("Enter your first name: ")
+lastname = input ("Enter your last name: ")
+street = input ("Enter your street address: ")
+city = input ("Enter your city: ")
+state = input ("Enter your address state: ") 
+zip = input ("Enter your zip code: ")
+print (firstname + lastname)
+print (street)
+print ( city + " " +  state + " " ++++++++++++++++++++++++++++++++ zip )
 
 print("########## 1.2.4 ##########")
 
-# Fix the code
-part = input("The 1st part: ")
-part = input("The 1st part: ")
-part = input("The 1st part: ")
-print(part + part + part)
+# Fix the codef
+fpart = input("The 1st part: ")
+spart = input("The 2nd part: ")
+tpart = input("The 3rd part: ")
+print(fpart + spart + tpart)
 
 print("########## 1.2.5 ##########")
+name = input ("Please enter name:")
+year = input ("Please enter year:")
+print (name + " is a valiant knight, born in the year " + year  + ". One morning " + name  + " woke up to an awful racket: a dragon was approaching the village. Only " + name + " could save the village's residents.")+
