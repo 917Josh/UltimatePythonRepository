ce8b227a26a367946d036c6a0b82dfd6d39fa793
917Josh <dj917josh@gmail.com>
2024-04-16 12:32:03 -0500

    did some work...

--- Unit2/3WhileTrueLoopsAssignment.py
+++ Unit2/3WhileTrueLoopsAssignment.py
@@ -1,25 +1,48 @@
 # ========== 2.3.1 ==========
-
-
+# while True:
+#     print ("hi")
+#     answer = input("Shall we continue? ")
+#     if answer == "no":
+#         break      
+# print ("okay then")
 # ========== 2.3.2 ==========
-
-
+# while True:
+#   number = float(input("Please type in a number: "))
+#   from math import sqrt
+#   if number > 0:  
+#     print(sqrt(number))
+#   else: 
+#       print ("Invalid number")
+#   if number == 0:
+#      break
+# print ("Exiting...")
 # ========== 2.3.3 ==========
 # number = 5
 # print("Countdown!")
 # while True:
 #   print(number)
-#   number = number - 1
-#   if number > 0:
-#     break
-#
+#   if number == 1:
+#      print (countdown)
+#   break
 # print("Now!")
-
 # ========== 2.3.4 ==========
-
-
+# password = input("Password: ")
+# while True:
+#   passwordrepeat = input("Repeat password: ")
+#   if password != passwordrepeat:
+#    print("They do not match!")      
+#   else:
+#     break
+# print("User account created!")
 # ========== 2.3.5 ==========
-
+while True:
+    pin = 4321
+    pininput = int(input("PIN: "))
+    if pininput != pin:
+        print("Wrong")
+    else:
+        break
+print("Correct! It only took you", "attempts")
 
 # ========== 2.3.6 ==========
 
