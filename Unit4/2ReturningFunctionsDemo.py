fc13942d02d8d03b9c95456a4209876d35d76fbd
917Josh <dj917josh@gmail.com>
2024-05-20 11:56:36 -0500

    commit

--- Unit4/2ReturningFunctionsDemo.py
+++ Unit4/2ReturningFunctionsDemo.py
@@ -1,18 +1,34 @@
 # ========== 4.2.1 ==========
-def greatest_number(num1, num2, num3):
-    if num1 > num2 and num1 > num3:
-       return(num1)
-    elif num2 > num1 and num2 > num3:
-        return(num2)
-    else:
-        return(num3)
-print(greatest_number(3, 4, 1))
-print(greatest_number(99, -4, 7))
-print(greatest_number(0, 0, 0)) 
+# def greatest_number(num1, num2, num3):
+#     if num1 > num2 and num1 > num3:
+#        return(num1)
+#     elif num2 > num1 and num2 > num3:
+#         return(num2)
+#     else:
+#         return(num3)
+# print(greatest_number(3, 4, 1))
+# print(greatest_number(99, -4, 7))
+# print(greatest_number(0, 0, 0)) 
 # ========== 4.2.2 ==========
+def same_chars(word, index1, index2):
+    # check here if index1 or index2 is too large!
+    if index1 > len(word) or index2 > len(word):
+        return("the second index is not within the string")
 
+    letter1 = word[index1]
+    letter2 = word[index2]
 
-
+    # now, check if they're equal
+    if letter1 == letter2:
+        return("True")
+    elif index1 > len(word) or index2 > len(word):
+        return("the second index is not within the string")
+    else:
+        return("False")
+    
+print(same_chars("programmer", 6, 7)) # True
+print(same_chars("programmer", 0, 4)) # False
+print(same_chars("programmer", 0, 12)) # False
 # ========== 4.2.3 ==========
 
 
--- Unit4/3ListsIntroAssignment.py
+++ Unit4/3ListsIntroAssignment.py
@@ -1,8 +1,20 @@
 # ========== 4.3.1 ==========
-
-
+my_list = [1, 2, 3, 4, 5]
+while True:
+    index = int(input("Please put in a new index"))
+    if index == -1:
+        break
+    value = int(input("please type in a new value"))
+    my_list[index] = value
+    print(my_list)
 # ========== 4.3.2 ==========
+items = int(input("How items:"))
+counter = 0
+numbers = []
 
+while True:
+    counter = counter + 1
+    item value = int
 
 # ========== 4.3.3 ==========
 
